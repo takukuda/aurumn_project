2f837333b8f1610c701e098b4e4645626bdd4f0a
takukuda <167833203+takukuda@users.noreply.github.com>
2025-01-26 01:00:34 +0900

    Update image.js

--- image.js
+++ image.js
@@ -4,4 +4,6 @@ function imgchange3(){document.getElementById('logo').src='./image/buranko_girl_
                    setTimeout("imgchange4()",3000);}
 
 function imgchange4(){document.getElementById('logo').src='./image/buranko_girl_sad.png';}
-function imgchange5(){document.getElementById("logo").classList.add("imagechange");}
+function imgchange5(){document.getElementById("logo").classList.add("imagechange1");}
+function imgchange6(){document.getElementById("logo").classList.add("imagechange2");}
+function imgchange7(){document.getElementById("logo").classList.remove("imagechange2");}
