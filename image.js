5f12a3785e99ee6c4e94484f4be9b3bf6378db34
takukuda <167833203+takukuda@users.noreply.github.com>
2025-01-26 01:17:44 +0900

    Update image.js

--- image.js
+++ image.js
@@ -1,9 +1,12 @@
 function imgchange1(){document.getElementById('logo').src='./image/buranko_girl_smile.png';}
 function imgchange2(){document.getElementById('logo').src='./image/buranko_girl_sad.png';}
 function imgchange3(){document.getElementById('logo').src='./image/buranko_girl_smile.png';
-                   setTimeout("imgchange4()",3000);}
+                   setTimeout("imgchange2()",3000);}
 
-function imgchange4(){document.getElementById('logo').src='./image/buranko_girl_sad.png';}
-function imgchange5(){document.getElementById("logo").classList.remove("imagechange2"); document.getElementById("logo").classList.add("imagechange1");}
-function imgchange6(){document.getElementById("logo").classList.remove("imagechange1"); document.getElementById("logo").classList.add("imagechange2");}
-function imgchange7(){document.getElementById("logo").classList.remove("imagechange1"); document.getElementById("logo").classList.remove("imagechange2");}
+function imgchange4(){document.getElementById("logo").classList.remove("imagechange2"); document.getElementById("logo").classList.add("imagechange1");}
+function imgchange5(){document.getElementById("logo").classList.remove("imagechange1"); document.getElementById("logo").classList.add("imagechange2");}
+function imgchange6(){document.getElementById("logo").classList.remove("imagechange1"); document.getElementById("logo").classList.remove("imagechange2");}
+function imgchange7(){document.getElementById("logo").classList.remove("imagechange2"); document.getElementById("logo").classList.add("imagechange1");
+                   setTimeout("imgchange6()",3000);}
+function imgchange7(){document.getElementById("logo").classList.remove("imagechange1"); document.getElementById("logo").classList.add("imagechange2");
+                   setTimeout("imgchange6()",3000);}
