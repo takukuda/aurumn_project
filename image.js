1b935c32e1f2a55c7810470ed57b43060c4d1ab4
takukuda <167833203+takukuda@users.noreply.github.com>
2025-01-25 23:45:24 +0900

    Create image.js

---
+++ image.js
@@ -0,0 +1,2 @@
+function imgchange1(){document.getElementById('logo').src='./image/buranko_girl_smile.png';}
+function imgchange2(){document.getElementById('logo').src='./image/buranko_girl_sad.png';}
